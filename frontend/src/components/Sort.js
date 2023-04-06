e41c195b931b477f47376dd4b11df7331d4a692a
CrioUser <crio-user@criodo.com>
2023-04-06 14:11:40 +0530

    ME_BUILDOUT_XFLIX_REACT_MODULE_REACT

--- frontend/src/components/Sort.js
+++ frontend/src/components/Sort.js
@@ -6,9 +6,10 @@ import axios from "axios";
 import {config} from "../App"
 
 function Sort({setVideos}){
-    // const [sort, setSort] = useState("");
+  const [sort, setSort] = useState("releaseDate");
 
     let handleChange = async(e) =>{
+      setSort(e.target.value);
       try {
         console.log(`${config.endpoint}/videos?sortBy=${e.target.value}`)
         let res = await axios.get(`${config.endpoint}/videos?sortBy=${e.target.value}`)
@@ -20,22 +21,20 @@ function Sort({setVideos}){
     }
 
     return(
-    <FormControl fullWidth>
-    <InputLabel id="demo-simple-select-label" className="labe">Sort by</InputLabel>
-    <Select
-      className="select"
-      labelId="demo-simple-select-label"
-      sx={{ height: 35, width: 180, borderRadius: 6 }}
-      onChange = {(e)=>{handleChange(e)}}
-    >
-      <MenuItem value="releaseDate" id="release-date-option">
-        Sort By: Release Date
-      </MenuItem>
-      <MenuItem value="viewCount" id="view-count-option">
-        Sort By: View Count
-      </MenuItem>
-    </Select>
-    </FormControl>
+      <Select
+        value={sort}
+        className="sort-select"
+        // startDecorator={<ImportExportIcon color="white" />}
+        onChange={(e) => handleChange(e)}
+        sx={{ height: 35, width: 180, borderRadius: 6 }}
+      >
+        <MenuItem value="releaseDate" id="release-date-option">
+          Sort By: Release Date
+        </MenuItem>
+        <MenuItem value="viewCount" id="view-count-option">
+          Sort By: View Count
+        </MenuItem>
+      </Select>
     )
 }
 
